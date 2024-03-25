d6b918c17f426edf6448843890760f2696a898c6
Andrey Voronin <AVor0n@yandex.ru>
2024-03-25 15:55:59 +0300

    feat(front): добавление Layout

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,31 +1,12 @@
 import { ThemeProvider, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
 import './global.css';
-import { toaster } from '@gravity-ui/uikit/toaster-singleton-react-18';
-import { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
 import { RouterProvider } from './providers/RouterProvider/RouterProvider';
 
-export const App = () => {
-  useEffect(() => {
-    toaster.add({
-      name: 'hello',
-      theme: 'success',
-      title: 'Run successful',
-      content: 'Hello from Frontend App!',
-    });
-  }, []);
-
-  return (
-    <ThemeProvider theme="light">
-      <ToasterProvider>
-        <div style={{ display: 'flex', gap: '10px' }}>
-          <NavLink to="/">Главная</NavLink>
-          <NavLink to="/auth">Авторизация</NavLink>
-          <NavLink to="/homeworks">Домашки</NavLink>
-        </div>
-        <RouterProvider />
-        <ToasterComponent />
-      </ToasterProvider>
-    </ThemeProvider>
-  );
-};
+export const App = () => (
+  <ThemeProvider theme="light">
+    <ToasterProvider>
+      <RouterProvider />
+      <ToasterComponent />
+    </ToasterProvider>
+  </ThemeProvider>
+);
