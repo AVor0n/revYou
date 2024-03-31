d338ac99fe0f08531dec211af189e06df8eb86f0
Znay Artemiy <artem.znay@mail.ru>
2024-03-31 22:21:43 +0300

    delete unnecessary files

--- frontend/src/pages/AuthPage/index.ts
+++ frontend/src/pages/AuthPage/index.ts
@@ -1,3 +1,2 @@
 export { AuthPage } from './AuthPage';
-export { authReducer } from './slice/Auth.slice';
 export { type AuthSchema } from './types/Auth.types';
--- frontend/src/pages/AuthPage/selectors/getAuthData.ts
+++
@@ -1,4 +0,0 @@
-// import { type StoreSchema } from 'app';
-
-// export const getAuthLogin = (state: StoreSchema) => state.auth.login || '';
-// export const getAuthPassword = (state: StoreSchema) => state.auth.password || '';
