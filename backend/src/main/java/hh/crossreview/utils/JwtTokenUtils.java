d3eeb526ba85ae7b6763519d45ba9101828b6773
YauheniSA <SemashkevicEA@yandex.ru>
2024-04-04 22:37:20 +0300

    registration and authentication after review

--- backend/src/main/java/hh/crossreview/utils/JwtTokenUtils.java
+++ backend/src/main/java/hh/crossreview/utils/JwtTokenUtils.java
@@ -9,14 +9,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import jakarta.inject.Named;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
-
-
-@Component
+@Named
 public class JwtTokenUtils {
   @Value("${jwt.secret}")
   private String secret;
