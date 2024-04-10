568b54800bc8de6d651c79af553284603202bb0e
Arseny Bazhenov <gorbaty.kit@gmail.com>
2024-04-10 23:47:49 +0300

    fix swagger configuration

--- backend/src/main/java/hh/crossreview/configuration/JerseyConfiguration.java
+++ backend/src/main/java/hh/crossreview/configuration/JerseyConfiguration.java
@@ -1,8 +1,17 @@
 package hh.crossreview.configuration;
 
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import jakarta.annotation.PostConstruct;
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.ServerErrorException;
+import jakarta.ws.rs.core.Response;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +26,32 @@ public class JerseyConfiguration extends ResourceConfig {
         "hh.crossreview.resource"
     );
     register(OpenApiResource.class);
+    configureSwagger();
+  }
+
+  private void configureSwagger() {
+    SwaggerConfiguration oasConfig = createSwaggerConfiguration();
+    try {
+      new JaxrsOpenApiContextBuilder<>()
+          .openApiConfiguration(oasConfig)
+          .buildContext(true);
+    } catch (OpenApiConfigurationException e) {
+      throw new ServerErrorException(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private SwaggerConfiguration createSwaggerConfiguration() {
+    return new SwaggerConfiguration()
+        .openAPI(new OpenAPI()
+            .info(new Info()
+                .title("Cross Review OpenAPI")
+                .version("0.0")
+                .description("Specification for cross-review service"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"))));
   }
 
 }
