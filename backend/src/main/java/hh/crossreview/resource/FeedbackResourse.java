4c5288594c86a8ba4761a56524cc1dd72e40e827
Nifpd <naidanov_p@mail.ru>
2024-05-24 00:29:41 +0500

    Поправил фидбэк

--- backend/src/main/java/hh/crossreview/resource/FeedbackResourse.java
+++ backend/src/main/java/hh/crossreview/resource/FeedbackResourse.java
@@ -1,16 +1,19 @@
 package hh.crossreview.resource;
 
+import hh.crossreview.dto.exception.ExceptionValidationDto;
+import hh.crossreview.dto.feedback.FeedbackDto;
 import hh.crossreview.dto.feedback.FeedbackPostDto;
+import hh.crossreview.dto.feedback.FeedbackWrapperDto;
+import hh.crossreview.dto.review.ReviewWrapperDto;
 import hh.crossreview.service.FeedbackService;
 import hh.crossreview.utils.JwtTokenUtils;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -33,20 +36,36 @@ public class FeedbackResourse {
   @GET
   @Path("{feedbackId}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getHomework(@PathParam("feedbackId") Integer feedbackId) {
+  @ApiResponse(responseCode = "200",
+          description = "Successful operation",
+          content = @Content(schema = @Schema(implementation = FeedbackDto.class)))
+  public Response getFeedback(@PathParam("feedbackId") Integer feedbackId) {
     var feedbackDto = feedbackService.getFeedback(feedbackId);
     return Response.status(Response.Status.OK).entity(feedbackDto).build();
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
+  @ApiResponse(
+          responseCode = "200",
+          description = "Successful operation",
+          content = @Content(schema = @Schema(implementation = FeedbackWrapperDto.class)))
   public Response getFeedbacks() {
     var feedbacks = feedbackService.getFeedbacks();
-    return Response.status(Response.Status.OK).entity(feedbacks).build();
+    var feedbacksWrapperDto = new FeedbackWrapperDto(feedbacks);
+    return Response.status(Response.Status.OK).entity(feedbacksWrapperDto).build();
   }
 
   @POST
   @Produces(MediaType.APPLICATION_JSON)
+  @ApiResponse(
+          responseCode = "201",
+          description = "Created",
+          content = @Content(schema = @Schema(implementation = FeedbackPostDto.class)))
+  @ApiResponse(
+          responseCode = "400",
+          description = "Bad request",
+          content = @Content(schema = @Schema(implementation = ExceptionValidationDto.class)))
   public Response createFeedback(FeedbackPostDto feedbackPostDto, @Context SecurityContext securityContext) {
     var feedback = feedbackService.createFeedback(feedbackPostDto, jwtTokenUtils.retrieveTokenFromContext(securityContext));
     return Response.status(Response.Status.CREATED).entity(feedback).build();
